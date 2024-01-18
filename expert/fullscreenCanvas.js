6862bc8376f4f05b496da61f7e27c42b1e75f717
Ian Marshall <ian.js.tutor@gmail.com>
2024-01-18 08:48:03 -0500

    better lifecycle hooks

--- expert/fullscreenCanvas.js
+++ expert/fullscreenCanvas.js
@@ -1,4 +1,5 @@
-var callback = null;
+var callbackBefore = null;
+var callbackAfter = null;
 var debouncer = null;
 var debounceDelay = 100; //milliseconds
 
@@ -13,7 +14,8 @@ function fullscreenCanvas(canvas, window) {
             !canvas.height) throw "Not a canvas element";
 
         resize();
-        window.addEventListener("resize", () => {
+        window.addEventListener("resize", e => {
+            if (callbackBefore) callbackBefore(e);
             if (!debounceDelay) return resize(); //a debounceDelay of zero disables debouncing
             if (debouncer) clearTimeout(debouncer);
             debouncer = setTimeout(resize, debounceDelay);
@@ -23,7 +25,7 @@ function fullscreenCanvas(canvas, window) {
             const { innerWidth, innerHeight } = window;
             canvas.width = innerWidth;
             canvas.height = innerHeight;
-            if (callback) callback({ width: innerWidth, height: innerHeight });
+            if (callbackAfter) callbackAfter({ width: innerWidth, height: innerHeight });
         }
 
         return canvas.getContext("2d");
@@ -33,9 +35,14 @@ function fullscreenCanvas(canvas, window) {
     }
 }
 
-function onResize(fn) {
-    if ("function" !== typeof fn) return console?.error?.("fullscreenCanvas error:", "Argument to setResizeCallback() must be a function");
-    callback = fn;
+function onBeforeResize(fn) {
+    if ("function" !== typeof fn) return console?.error?.("fullscreenCanvas error:", "Argument to onBeforeResize() must be a function");
+    callbackBefore = fn;
+}
+
+function onAfterResize(fn) {
+    if ("function" !== typeof fn) return console?.error?.("fullscreenCanvas error:", "Argument to onAfterResize() must be a function");
+    callbackAfter = fn;
 }
 
 /* setting ms to zero disables debouncing */
@@ -44,4 +51,4 @@ function setDebounceDelay(ms) {
     debounceDelay = Math.abs(ms);
 }
 
-export { fullscreenCanvas, onResize, setDebounceDelay };
+export { fullscreenCanvas, onBeforeResize, onAfterResize, setDebounceDelay };
